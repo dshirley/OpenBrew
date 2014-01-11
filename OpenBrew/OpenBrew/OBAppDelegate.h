26397e4ad8f4f5ae4da8f69453ec1b9b95256590
David Shirley <dshirley@crittercism.com>
2014-01-10 20:00:07 -0800

    Created default blank Xcode project

---
+++ OpenBrew/OpenBrew/OBAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  OBAppDelegate.h
+//  OpenBrew
+//
+//  Created by David Shirley 2 on 1/10/14.
+//  Copyright (c) 2014 OpenBrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
