27c700b449c57ce9e2106dc24eb7d5427920c044
David Shirley <dshirley@crittercism.com>
2014-01-14 09:19:03 -0800

    Added OBBrewHouseSettings class for storing setup constants

---
+++ OpenBrew/OpenBrew/OBBrewHouseSettings.h
@@ -0,0 +1,13 @@
+//
+//  OBBrewHouseSettings.h
+//  OpenBrew
+//
+//  Created by David Shirley 2 on 1/14/14.
+//  Copyright (c) 2014 OpenBrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OBBrewHouseSettings : NSObject
+
+@end
---
+++ OpenBrew/OpenBrew/OBBrewHouseSettings.m
@@ -0,0 +1,13 @@
+//
+//  OBBrewHouseSettings.m
+//  OpenBrew
+//
+//  Created by David Shirley 2 on 1/14/14.
+//  Copyright (c) 2014 OpenBrew. All rights reserved.
+//
+
+#import "OBBrewHouseSettings.h"
+
+@implementation OBBrewHouseSettings
+
+@end
