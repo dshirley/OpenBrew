c40f89b535657981e0b818daa65a1ca4d7d59bc5
David Shirley <dshirley@crittercism.com>
2014-02-04 12:17:32 +0900

    Added a controller for the ingredient finder view

---
+++ OpenBrew/OpenBrew/OBIngredientFinderViewController.h
@@ -0,0 +1,13 @@
+//
+//  OBIngredientFinderViewController.h
+//  OpenBrew
+//
+//  Created by David Shirley 2 on 2/4/14.
+//  Copyright (c) 2014 OpenBrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OBIngredientFinderViewController : UITableViewController <UITableViewDataSource>
+
+@end
