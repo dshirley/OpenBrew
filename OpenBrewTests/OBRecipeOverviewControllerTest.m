b759773ef7b42c5f3259cfaedacb592b7faf11d3
David Shirley <dshirley@crittercism.com>
2015-07-23 08:37:08 -0700

    Testing statistics view loading in OBRecipeOverviewControllerTest

--- OpenBrewTests/OBRecipeOverviewControllerTest.m
+++ OpenBrewTests/OBRecipeOverviewControllerTest.m
@@ -13,6 +13,15 @@
 
 @interface OBRecipeOverviewControllerTest : OBBaseTestCase
 @property (nonatomic) OBRecipeOverviewController *vc;
+
+// TODO: put these in a super class for convenience
+@property (nonatomic) NSIndexPath *r0s0;
+@property (nonatomic) NSIndexPath *r1s0;
+@property (nonatomic) NSIndexPath *r2s0;
+@property (nonatomic) NSIndexPath *r3s0;
+@property (nonatomic) NSIndexPath *r4s0;
+@property (nonatomic) NSIndexPath *r5s0;
+
 @end
 
 @implementation OBRecipeOverviewControllerTest
@@ -23,6 +32,13 @@
   UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
   self.vc = [storyboard instantiateViewControllerWithIdentifier:@"recipeOverview"];
   self.vc.recipe = self.recipe;
+
+  self.r0s0 = [NSIndexPath indexPathForRow:0 inSection:0];
+  self.r1s0 = [NSIndexPath indexPathForRow:1 inSection:0];
+  self.r2s0 = [NSIndexPath indexPathForRow:2 inSection:0];
+  self.r3s0 = [NSIndexPath indexPathForRow:3 inSection:0];
+  self.r4s0 = [NSIndexPath indexPathForRow:4 inSection:0];
+  self.r5s0 = [NSIndexPath indexPathForRow:5 inSection:0];
 }
 
 - (void)tearDown {
@@ -34,63 +50,59 @@
   [self.vc performSelectorOnMainThread:@selector(loadView) withObject:nil waitUntilDone:YES];
 }
 
-- (void)testLoadingEmptyRecipe {
+- (void)testLoadingTableViewEmptyRecipe
+{
   [self loadView];
-
-  NSIndexPath *r0s0 = [NSIndexPath indexPathForRow:0 inSection:0];
-  NSIndexPath *r1s0 = [NSIndexPath indexPathForRow:1 inSection:0];
-  NSIndexPath *r2s0 = [NSIndexPath indexPathForRow:2 inSection:0];
-  NSIndexPath *r3s0 = [NSIndexPath indexPathForRow:3 inSection:0];
-  NSIndexPath *r4s0 = [NSIndexPath indexPathForRow:2 inSection:0];
-  NSIndexPath *r5s0 = [NSIndexPath indexPathForRow:3 inSection:0];
 
   XCTAssertEqual(4, [self.vc.tableView numberOfRowsInSection:0]);
   XCTAssertEqual(1, [self.vc.tableView numberOfSections]);
 
   UITableViewCell *tCell = nil;
-  tCell = [self.vc.tableView cellForRowAtIndexPath:r0s0];
+
+  tCell = [self.vc tableView:self.vc.tableView cellForRowAtIndexPath:self.r0s0];
   XCTAssertEqualObjects(@"Batch size", tCell.textLabel.text);
   XCTAssertEqualObjects(@"6.0 gallons", tCell.detailTextLabel.text);
 
-  tCell = [self.vc.tableView cellForRowAtIndexPath:r1s0];
+  tCell = [self.vc tableView:self.vc.tableView cellForRowAtIndexPath:self.r1s0];
   XCTAssertEqualObjects(@"Malts", tCell.textLabel.text);
   XCTAssertEqualObjects(@"0 varieties", tCell.detailTextLabel.text);
 
-  tCell = [self.vc.tableView cellForRowAtIndexPath:r2s0];
+  tCell = [self.vc tableView:self.vc.tableView cellForRowAtIndexPath:self.r2s0];
   XCTAssertEqualObjects(@"Hops", tCell.textLabel.text);
   XCTAssertEqualObjects(@"0 varieties", tCell.detailTextLabel.text);
 
-  tCell = [self.vc.tableView cellForRowAtIndexPath:r3s0];
+  tCell = [self.vc tableView:self.vc.tableView cellForRowAtIndexPath:self.r3s0];
   XCTAssertEqualObjects(@"Yeast", tCell.textLabel.text);
   XCTAssertEqualObjects(@"none", tCell.detailTextLabel.text);
+}
 
-  XCTAssertEqual(6, [self.vc.collectionView numberOfItemsInSection:0]);
-  XCTAssertEqual(1, [self.vc.collectionView numberOfSections]);
+- (void)testCollectionViewEmptyRecipe
+{
+  [self loadView];
 
-  // FIXME: For some reason cellForItemAtIndexPath returns nil and doesn't call
-  // the delegate method in the controller. The delegate & data source are
-  // both set when I debug, though.
+  XCTAssertEqual(6, [self.vc collectionView:self.vc.collectionView numberOfItemsInSection:0]);
 
-//  OBTextStatisticsCollectionViewCell *statsCell = nil;
-//  statsCell = (id)[self.vc.collectionView cellForItemAtIndexPath:r0s0];
-//  XCTAssertEqualObjects(@"1.000", statsCell.statisticLabel.text);
-//  XCTAssertEqualObjects(@"Original gravity", statsCell.descriptionLabel.text);
-//
-//  statsCell = (id)[self.vc.collectionView cellForItemAtIndexPath:r1s0];
-//  XCTAssertEqualObjects(@"1.000", statsCell.statisticLabel.text);
-//  XCTAssertEqualObjects(@"Final gravity", statsCell.descriptionLabel.text);
-//
-//  statsCell = (id)[self.vc.collectionView cellForItemAtIndexPath:r2s0];
-//  XCTAssertEqualObjects(@"0.0%", statsCell.statisticLabel.text);
-//  XCTAssertEqualObjects(@"ABV", statsCell.descriptionLabel.text);
-//
-//  statsCell = (id)[self.vc.collectionView cellForItemAtIndexPath:r4s0];
-//  XCTAssertEqualObjects(@"0", statsCell.statisticLabel.text);
-//  XCTAssertEqualObjects(@"IBU", statsCell.descriptionLabel.text);
-//
-//  statsCell = (id)[self.vc.collectionView cellForItemAtIndexPath:r5s0];
-//  XCTAssertEqualObjects(@"inf", statsCell.statisticLabel.text);
-//  XCTAssertEqualObjects(@"BU:GU", statsCell.descriptionLabel.text);
+  OBTextStatisticsCollectionViewCell *statsCell = nil;
+
+  statsCell = (id)[self.vc collectionView:self.vc.collectionView cellForItemAtIndexPath:self.r0s0];
+  XCTAssertEqualObjects(@"1.000", statsCell.statisticLabel.text);
+  XCTAssertEqualObjects(@"Original gravity", statsCell.descriptionLabel.text);
+
+  statsCell = (id)[self.vc collectionView:self.vc.collectionView cellForItemAtIndexPath:self.r1s0];
+  XCTAssertEqualObjects(@"1.000", statsCell.statisticLabel.text);
+  XCTAssertEqualObjects(@"Final gravity", statsCell.descriptionLabel.text);
+
+  statsCell = (id)[self.vc collectionView:self.vc.collectionView cellForItemAtIndexPath:self.r2s0];
+  XCTAssertEqualObjects(@"0.0%", statsCell.statisticLabel.text);
+  XCTAssertEqualObjects(@"ABV", statsCell.descriptionLabel.text);
+
+  statsCell = (id)[self.vc collectionView:self.vc.collectionView cellForItemAtIndexPath:self.r4s0];
+  XCTAssertEqualObjects(@"0", statsCell.statisticLabel.text);
+  XCTAssertEqualObjects(@"IBU", statsCell.descriptionLabel.text);
+
+  statsCell = (id)[self.vc collectionView:self.vc.collectionView cellForItemAtIndexPath:self.r5s0];
+  XCTAssertEqualObjects(@"inf", statsCell.statisticLabel.text);
+  XCTAssertEqualObjects(@"BU:GU", statsCell.descriptionLabel.text);
 }
 
 @end
