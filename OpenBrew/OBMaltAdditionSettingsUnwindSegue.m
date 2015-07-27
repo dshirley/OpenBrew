c51074f240b6f79ff4d4185165d8fdf1252379cb
David Shirley <dshirley@crittercism.com>
2015-07-26 18:03:14 -0700

    pretty good animation of unwind

--- OpenBrew/OBMaltAdditionSettingsUnwindSegue.m
+++ OpenBrew/OBMaltAdditionSettingsUnwindSegue.m
@@ -7,11 +7,44 @@
 //
 
 #import "OBMaltAdditionSettingsUnwindSegue.h"
+#import "OBMaltAdditionViewController.h"
+#import "OBMaltAdditionSettingsViewController.h"
 
 @implementation OBMaltAdditionSettingsUnwindSegue
 
-- (void)perform {
-   [self.sourceViewController dismissViewControllerAnimated:NO completion:NULL];
+- (void)perform
+{
+  OBMaltAdditionSettingsViewController *settingsVc = (id)self.sourceViewController;
+  OBMaltAdditionViewController *maltsVc = (id)self.destinationViewController;
+//  [maltsVc.navigationItem setHidesBackButton:YES animated:YES];
+
+//  settingsVc.navigationBar.frame = maltsVc.navigationController.navigationBar.frame;
+//  [maltsVc.view bringSubviewToFront:settingsVc.navigationBar];
+
+  [maltsVc.navigationItem setHidesBackButton:NO animated:YES];
+
+  UIBarButtonItem *addPlaceholder = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:nil action:nil];
+//  [settingsVc.navigationBar.topItem setRightBarButtonItem:addPlaceholder animated:YES];
+
+  [UIView animateWithDuration:0.5
+                        delay:0.0
+                      options:0
+                   animations:^{
+
+                     CGRect settingsFrame = settingsVc.settingsView.frame;
+                     settingsVc.settingsView.frame = CGRectOffset(settingsFrame, 0,
+                                                                  settingsFrame.size.height);
+
+                     settingsVc.greyoutView.backgroundColor = [UIColor clearColor];
+                   }
+                   completion:^(BOOL finished) {
+                     [self.sourceViewController dismissViewControllerAnimated:NO
+                                                                   completion:NULL];
+                   }];
+
+
+
+
 }
 
 @end
